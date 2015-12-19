2d0b2cdff5f663c4654b623d9e8c3adb7b066dc5
nhadzariga <n.hadzariga@gmail.com>
2015-12-19 10:19:57 +0000

    minor adjustments.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,5 +3,7 @@ class HomeController < ApplicationController
     @env = Rails.env
     @price = Product.first.price
     @testimonials = Testimonial.all
+    @total_qty_ordered = Order.sum(:qty)
+    @latest_orders = Order.order(created_at: :desc).limit(5)
   end
 end
